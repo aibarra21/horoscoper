b0ff0cd6c228967dd89abca4f2c2db0cff83642b
rebeccaioffe <rioffe@calpoly.edu>
2024-05-29 07:44:30 -0700

    compatibility_function
    compatibility of two signs

--- R/compatibility.R
+++ R/compatibility.R
@@ -8,89 +8,82 @@
 #@ Regardless of capitalization, but spelling important
 
 # @return A compatibility message string from webscraped website
-# Url https://www.horoscope.com/us/index.aspx
-#https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=12
-
 # @export
 
 library(tidyverse)
 library(rvest)
+library(stringr)
+
+compatibility <- function(sign1, sign2) {
+  if (sign1 %in% comp_data$sign_items1 || sign2 %in% comp_data$sign_items2){
+    matched_row <- comp_data %>%
+      filter(sign_items1 == sign_items2 & sign_items2 == sign_items2)
+
+    if (nrow(matched_row) > 0){
+      cat("Romantic Compatibility between", sign1, "and", sign2, ":\n")
+      cat("Compatibility Score out of 10:", matched_row$percent, "\n")
+      cat("Description:", matched_row$selected_description, "\n")
+    }
+
+    else {
+      cat("Invalid Zodiac Signs entered. Please check for correct spelling and capitalization.\n")
+    }
+  }
+}
+
+scrape_comp_page <- function(sign_number, sign_number2) {
 
 
-#implement a compatibility scraping function as helper function
-#sign = 1:12 at the end of url
-#.list_item gives all sign compatibilities
-#make one df with all, keep unique
-
-sign_number <- 12
-
-scrape_comp_page <- function(sign_number) {
-
-  #consider pages for all 12 signs
-
-  url <- paste0("https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=", sign_number) #consider all sign pages
+  url <- paste0("https://www.horoscope.com/us/games/compatibility/game-love-compatibility.aspx?ZodiacSignSelector_alphastring=", sign_number, "&PartnerZodiacSignSelector_alphastring=", sign_number2) #consider all sign pages
   webpage <- read_html(url)
 
-  print(url)
 
+  #define compatibility items
 
-  #define sign items
+  sign_items1 <- webpage %>%
+    html_nodes(".flex-center-inline div:nth-child(1) h3") %>%
+    html_text(trim = FALSE)
 
-  sign_items <- webpage %>%
-    html_node(".list_item a") %>%
+  sign_items2 <- webpage %>%
+    html_nodes(".icon-heart + div h3") %>%
+    html_text(trim = FALSE)
+
+  compatibility_score <- webpage %>%
+    html_node("b") %>%
     html_text(trim = TRUE)
 
-  #initialize vectors
+  compatibility_desc <- webpage %>%
+    html_node(".text-center p") %>%
+    html_text(trim = TRUE)
 
-  sign_v_sign_names <- vector("character", length(sign_items))
-  compatibility_desc <- vector("character", length(sign_items))
-  compatibility_score <- vector("character", length(sign_items))
+  extract_description(compatibility_desc, n = 8)
 
-  # for each sign item:
+  compatibility_info <- data.frame(sign_items1, sign_items2, compatibility_score, compatibility_desc)
+  names(compatibility_info) <- c("sign_items1", "sign_items2", "compatibility_score", "selected_description")
+  return(compatibility_info)
 
-  for (x in seq_along(sign_items)) {
-    sign_v_sign_names[x] <- sign_items[x] %>%
-      html_node(".list_item") %>%
-        html_text(trim = true)
+}
 
-    # scrape compatibility attributes
 
-    compatibility_desc[x] <- sign_items[x] %>%
-      html_node("div p") %>% # or div p
-        html_text(trim = TRUE)
+extract_description <- function(text, n){
+  sentences <- stringr::str_extract_all(text, "(.*?[.!])", simplify = TRUE)[1:n]
+  return(paste(sentences, collapse = " "))
+}
 
-     compatibility_score[x] <- sign_items[x] %>%
-       html_node("div b") %>%
-        html_text(trim = TRUE)
+
+comp_data <- data.frame(signs = character(),
+                        compatibility_score = character(),
+                        selected_description = character(),
+                        stringsAsFactors = FALSE)
+for(z in 0:11){
+  for (x in 0:11){
+    compinfor <- scrape_comp_page(z,x)
+    comp_data <- rbind(comp_data, compinfor)
 
   }
-
-
-  #create a data frame for this sign page
-
-  data_frame(
-    SignvSign = sign_v_sign_names,
-    Url = urls
-  )
-
-  #return data_frame?
-
 }
 
 
-# create one data frame with all compatibilities
-
-all_comp <- map_df(1:12, scrape_comp_page)
-
-get_compatibility <- function(your_sign, their_sign) {
-
-  #check how sign names are formatted
-  #split sign v sign into two signs: 1 and 2
-  #then check for the presence of two in the get_compatibility function
-  #use helper function to check
 
 
 
-
-
-}
